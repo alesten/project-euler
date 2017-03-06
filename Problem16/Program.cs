f0c8b107af98b9a2db7989abfda2b4e3852be641
Alexander Steen <ale.steen@hotmail.com>
2017-03-06 20:44:03 +0100

    Added problem 16 and started on 10

---
+++ Problem16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = BigInteger.Pow(2, 1000);
+            var numbers = res.ToString().ToCharArray();
+
+            var result = 0;
+
+            foreach (var number in numbers)
+            {
+                result += int.Parse(number.ToString());
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
