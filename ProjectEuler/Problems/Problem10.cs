50f5167516f04cfd4fca6fd6cebc326e83dd97f9
Alexander Steen <ale.steen@hotmail.com>
2017-03-06 22:21:59 +0100

    WIP: Problem 10

--- ProjectEuler/Problems/Problem10.cs
+++ ProjectEuler/Problems/Problem10.cs
@@ -1,10 +1,57 @@
-﻿namespace ProjectEuler.Problems
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace ProjectEuler.Problems
 {
-    public class Problem10 : IProblem 
+    public class Problem10 : IProblem
     {
+
         public string CalculateProblem()
         {
-            throw new System.NotImplementedException();
+            return FindPrimes(20000000).Aggregate((sum, x) => sum + x).ToString();
+        }
+
+        private List<ulong> FindPrimes(ulong limit)
+        {
+            List<ulong> primes = new List<ulong>();
+
+            var isPrime = new bool[limit + 1];
+            var sqrt = Math.Sqrt(limit);
+
+            for (ulong x = 1; x <= sqrt; x++)
+                for (ulong y = 1; y <= sqrt; y++)
+                {
+                    var n = 4*x*x + y*y;
+                    if (n <= limit && (n%12 == 1 || n%12 == 5))
+                        isPrime[n] ^= true;
+
+                    n = 3*x*x + y*y;
+                    if (n <= limit && n%12 == 7)
+                        isPrime[n] ^= true;
+
+                    n = 3*x*x - y*y;
+                    if (x > y && n <= limit && n%12 == 11)
+                        isPrime[n] ^= true;
+                }
+
+            for (ulong n = 5; n <= sqrt; n++)
+                if (isPrime[n])
+                {
+                    var s = n*n;
+                    for (ulong k = s; k <= limit; k += s)
+                        isPrime[k] = false;
+                }
+
+            primes.Add(2);
+            primes.Add(3);
+            for (ulong n = 5; n <= limit; n += 2)
+                if (isPrime[n])
+                    primes.Add(n);
+
+            return primes;
         }
     }
 }
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -11,9 +11,24 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            var problem = new Problem16();
+            var problemNumber = 10;
 
-            Console.WriteLine(problem.CalculateProblem());
+            var problem = GetProblem(problemNumber);
+
+            Console.WriteLine($"Result of problem {problemNumber} is: {problem.CalculateProblem()}");
+        }
+
+        private static IProblem GetProblem(int i)
+        {
+            switch (i)
+            {
+                case 10:
+                    return new Problem10();
+                case 16:
+                    return new Problem16();
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
