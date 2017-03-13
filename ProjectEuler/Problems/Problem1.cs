56e6b40b8eb950bac2677558aac94c03777f31b0
Alexander Steen <ale.steen@hotmail.com>
2017-03-13 11:38:22 +0100

    Completed problem 1

---
+++ ProjectEuler/Problems/Problem1.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectEuler.Problems
+{
+    public class Problem1 : IProblem
+    {
+        public string CalculateProblem()
+        {
+            var sum = 0;
+            var i = 0;
+            while (i < 1000)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+
+                i++;
+            }
+
+            return sum.ToString();
+        }
+    }
+}
